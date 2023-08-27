320c205128e14588a79b568f4a1d5cb732a32288
zakaria ahmed <116184228+zachafs@users.noreply.github.com>
2023-08-27 13:13:45 +0100

    Update 0-fizzbuzz.py
    aaaa

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -19,11 +19,11 @@ def fizzbuzz(n):
     for i in range(1, n + 1):
         if (i % 3) == 0 and (i % 5) == 0:
             tmp_result.append("FizzBuzz")
-        elif (i % 5) == 0:
-            tmp_result.append("Buzz")
         elif (i % 3) == 0:
             tmp_result.append("Fizz")
-        else:
+        elif (i % 5) == 0:
+            tmp_result.append("Buzz")
+        elif (i % 3) != 0 and (i % 5) != 0:
             tmp_result.append(str(i))
     print(" ".join(tmp_result))
 
